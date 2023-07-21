78938e058603479972aaf6a39150be393a1a0226
Prashasth Nair <prashasthnair1@gmail.com>
2023-07-21 18:39:30 +0530

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/Face_recognition/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/Face_recognition/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/Face_recognition/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/Face_recognition/models')
 ]).then(startVideo)
 
 function startVideo() {
